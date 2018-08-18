ad9e3b4cba328a9d1b07704d38371cbd348a9520
Jose Luis Mesa <joselume@gmail.com>
2018-08-18 06:23:09 -0500

    Addition of comment to the command design pattern

--- command-pattern/src/main/java/Program.java
+++ command-pattern/src/main/java/Program.java
@@ -1,6 +1,11 @@
 import com.design.patterns.client.Application;
 
 public class Program {
+	// The Button is the invoker. The command that is going to be executed by the
+	// Button is injected to it, so the Button doesn't know with commands will be excecuted.
+	// In this way the developer can build the button without worrying about the business logic,
+	// and the Button will be used from whatever context.
+	
 	public static void main (String [] args)
 	{
 		Application app = new Application();
